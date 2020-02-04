71949d3cd4a5c4030b97514259848288efa99ccf
tabuna <bliz48rus@gmail.com>
2020-02-05 00:09:53 +0300

    dump

--- src/Guide.php
+++ src/Guide.php
@@ -8,6 +8,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request as IlluminateRequest;
 use Illuminate\Pipeline\Pipeline;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Sajya\Server\Http\Parser;
 use Sajya\Server\Http\Request;
 use Sajya\Server\Http\Response;
@@ -15,6 +16,8 @@ use Sajya\Server\Lines\MethodDetect;
 use Sajya\Server\Lines\UsefulWork;
 use Sajya\Server\Lines\ValidationRequestFormat;
 use Sajya\Server\Lines\ValidationRequestParams;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class Guide
 {
@@ -80,9 +83,47 @@ class Guide
     public function findProcedure(Request $request): ?Procedure
     {
         return $this->map
+            ->whenEmpty(fn(Collection $collection) => $collection->merge($this->findProceduresClass()))
             ->map(fn($procedure) => !is_object($procedure) ? app()->make($procedure) : $procedure)
             ->filter(fn(Procedure $procedure) => $procedure::$name === $request->getMethod())
             ->first();
+    }
+
+
+    /**
+     * @param string $path
+     *
+     * @return Collection
+     */
+    public function findProceduresClass(string $path = 'Http/Procedures'): Collection
+    {
+        $namespace = app()->getNamespace();
+        $directory = app_path($path);
+
+        if (!is_dir($directory)) {
+            return collect();
+        }
+
+        $files = (new Finder())->in($directory)->files();
+
+        return collect($files)
+            ->map(fn(SplFileInfo $resource) => $this->createClassForResource($resource->getPathname(), $namespace))
+            ->filter(fn(SplFileInfo $resource) => is_subclass_of($resource, Procedure::class));
+    }
+
+    /**
+     * @param SplFileInfo $resource
+     * @param string      $namespace
+     *
+     * @return string
+     */
+    private function createClassForResource(SplFileInfo $resource, string $namespace)
+    {
+        return $namespace . str_replace(
+                ['/', '.php'],
+                ['\\', ''],
+                Str::after($resource->getPathname(), app_path() . DIRECTORY_SEPARATOR)
+            );
     }
 
     /**
