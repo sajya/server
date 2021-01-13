a904ead8a871b63458ad6130eb99611bd6865ae5
tabuna <tabuna@users.noreply.github.com>
2021-01-13 16:16:59 +0000

    Fix styling

--- src/Rules/Identifier.php
+++ src/Rules/Identifier.php
@@ -11,8 +11,9 @@ class Identifier implements Rule
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
+     * @param string $attribute
+     * @param mixed  $value
+     *
      * @return bool
      */
     public function passes($attribute, $value)
