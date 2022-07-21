400e0034ad9f097f30ca0a199b06342b05b28601
幸福的路痴 <maoxuner@126.com>
2022-07-21 16:15:14 +0800

    Style better catch arrangement

--- src/HandleProcedure.php
+++ src/HandleProcedure.php
@@ -13,14 +13,12 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\App;
 use Illuminate\Validation\ValidationException;
-use RuntimeException;
 use Sajya\Server\Exceptions\InternalErrorException;
 use Sajya\Server\Exceptions\InvalidParams;
 use Sajya\Server\Exceptions\RpcException;
 use Sajya\Server\Exceptions\RuntimeRpcException;
 use Sajya\Server\Facades\RPC;
 use Sajya\Server\Http\Request;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class HandleProcedure implements ShouldQueue
 {
@@ -59,32 +57,27 @@ class HandleProcedure implements ShouldQueue
             $parameters = RPC::bindResolve($this->procedure, $this->request->getParams());
 
             return App::call($this->procedure, $parameters);
-        } catch (HttpException | RuntimeException | Exception $exception) {
+        } catch (Exception $exception) {
             app()->get(ExceptionHandler::class)->report($exception);
 
             if ($exception instanceof RpcException) {
                 return $exception;
-            }
-
-            $message = $exception->getMessage();
-
-            $code = method_exists($exception, 'getStatusCode')
-                ? $exception->getStatusCode()
-                : $exception->getCode();
-
-            if ($exception instanceof ValidationException) {
+            } else if ($exception instanceof ValidationException) {
                 return new InvalidParams($exception->validator->errors()->toArray());
-            }
+            } else {
+                $code = method_exists($exception, 'getStatusCode')
+                    ? $exception->getStatusCode()
+                    : $exception->getCode();
 
-            if ($code === 500) {
-                return new InternalErrorException();
-            }
+                if ($code === 500) {
+                    return new InternalErrorException();
+                }
 
-            if (! is_int($code)) {
-                $code = -1;
+                return new RuntimeRpcException(
+                    $exception->getMessage(),
+                    is_int($code) ? $code : -1
+                );
             }
-
-            return new RuntimeRpcException($message, $code);
         }
     }
 }
